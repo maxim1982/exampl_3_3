5b50cb09a260f61f8e5402f88eff3746f8a905aa
maxim1982 <maxim_7@mail.ru>
2017-02-19 19:28:01 +0300

    Add files via upload

--- vk_primer.py
+++ vk_primer.py
@@ -2,28 +2,50 @@ import requests
 
 VERSION = '5.60'
 APP_ID = 5869677
+common_friend = {'друг': 0}
 
-access_token = 'bf0a786cb1fda3452856f0cb340f1fe239f6ac62a82bd20be090f26c6d7b3afc1a27a4107fc7930b959..' # здесь нужен свой токен
+access_token = '00b912605fa5a4232f30039cf3acbba106bb8ff34e56de46cf0759246adc370f9140bcfa3b2a2949b....' # здесь нужен свой токен
 params = {'access_token': access_token, 'v': VERSION}
 
-#response = requests.get('https://api.vk.com/method/friends.get', params)
-#print(response.json()['response']['items'])
-#for user_id in response.json()['response']['items']:
+# все друзья в контакте...
+response = requests.get('https://api.vk.com/method/friends.get', params)
 
-response = requests.get('https://api.vk.com/method/friends.getOnline', params)
+# пробую с онлайн друзьями
+#response = requests.get('https://api.vk.com/method/friends.getOnline', params)
+
 i = 0
-for user_id in response.json()['response']:
+print('my_friend:')
+for user_id in response.json()['response']['items']:
     i += 1
     get_friend = requests.get('https://api.vk.com/method/users.get', {'user_id': user_id})
-    # Получим список всех своих друзей:
-    print(i, 'friend:', get_friend.json()['response'])
+    # Получим список всех своих друзей(в контакте):
+    print(i, get_friend.json()['response'][0])
+
+    # внесем в словарь друзей
+    if user_id in common_friend:
+        common_friend[user_id] += 1
+    else:
+        common_friend[user_id] = 1
 
 i = 0
-for user_id in response.json()['response']:
+for user_id in response.json()['response']['items']:
     i += 1
     get_friend_friends = requests.get('https://api.vk.com/method/friends.get',
-                                      {'access_token': access_token, 'v': VERSION,'user_id': user_id})
+                                      {'access_token': access_token, 'v': VERSION, 'user_id': user_id})
     # Для каждого своего друга получим список его друзей:
-    print(i, 'friend: ', user_id, '\nfriend_friends: ', get_friend_friends.json()['response']['items'])
+    print(i, 'friend: ', user_id, '\nfriend_friends: ', get_friend_friends.json())
+
+    # внесем в словарь друзей
+    if user_id in common_friend:
+        common_friend[user_id] += 1
+    else:
+        common_friend[user_id] = 1
+
+print('common friends :')
+
+for key in sorted(common_friend, key=common_friend.get, reverse=True):
+    if common_friend[key] > 1:
+        print(key, common_friend[key])
+
 
 
